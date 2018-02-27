fd66a240317da053a0db556e3ae89762f4a8bdb7
wanglj <wanglj@gzyitop.com>
2018-02-27 15:21:16 +0800

    单例的懒加载以及线程安全实现

--- src/main/java/com/wang/se/designpattern/singleton/Car2.java
+++ src/main/java/com/wang/se/designpattern/singleton/Car2.java
@@ -3,14 +3,18 @@ package com.wang.se.designpattern.singleton;
 /**
  * Created by wang on 2017/4/12. 构造方法私有化，然后提供一个
  * 只给司机一辆车
- * 非懒加载，它基于 classloder 机制避免了多线程的同步问题，所以是线程安全的
+ *
+ * 懒加载、非线程安全
  */
-public class Car {
-    private static Car car=new Car();
-    private Car(){
+public class Car2 {
+    private static Car2 car;
+    private Car2(){
 
     }
-    public static Car getInstance(){
+    public static Car2 getInstance(){
+        if(car==null){
+            car=new Car2();
+        }
         return car;
     }
     public void run(){
--- src/main/java/com/wang/se/designpattern/singleton/Car3.java
+++ src/main/java/com/wang/se/designpattern/singleton/Car3.java
@@ -3,14 +3,18 @@ package com.wang.se.designpattern.singleton;
 /**
  * Created by wang on 2017/4/12. 构造方法私有化，然后提供一个
  * 只给司机一辆车
- * 非懒加载，它基于 classloder 机制避免了多线程的同步问题，所以是线程安全的
+ *
+ * 懒加载、因为加了synchronized关键字所以线程安全
  */
-public class Car {
-    private static Car car=new Car();
-    private Car(){
+public class Car3 {
+    private static Car3 car;
+    private Car3(){
 
     }
-    public static Car getInstance(){
+    public synchronized static Car3 getInstance(){
+        if(car==null){
+            car=new Car3();
+        }
         return car;
     }
     public void run(){
