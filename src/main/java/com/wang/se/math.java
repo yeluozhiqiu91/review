94350225c9331797d00c103be2ea307ba769c0ba
wanglj <wanglj@gzyitop.com>
2018-04-10 17:59:08 +0800

    <<左移，>>右移操作

---
+++ src/main/java/com/wang/se/math.java
@@ -0,0 +1,13 @@
+package com.wang.se;
+
+/**
+ * Created by wang on 2018/4/9.
+ */
+public class math {
+    public static void main(String[] args) {
+        int a=5>>1;         //右移一位，相当于除以2的一次方
+        int b=5<<1;         //左移一位，相当于乘以2的一次方
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+    }
+}
