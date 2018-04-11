0fd81b56ebc1e1d36fab515ba66b54a4aa616d2b
wanglj <wanglj@gzyitop.com>
2018-04-11 11:45:23 +0800

    使用lambda实现的策略模式

--- src/main/java/com/wang/se/designpattern/strategy/Test2.java
+++ src/main/java/com/wang/se/designpattern/strategy/Test2.java
@@ -2,6 +2,7 @@ package com.wang.se.designpattern.strategy;
 
 /**
  * Created by wang on 2018/4/11.
+ * 使用lambda实现的策略模式，无需创建具体的策略实现类了
  */
 public class Test2 {
     public static void main(String[] args) {
